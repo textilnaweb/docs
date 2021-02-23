312a1f7071d57162d9dab3fa0b9891dcafc04d2e
Bruno Gonçalves <bugotech@gmail.com>
2021-02-23 09:27:51 -0300

    Mudando o favicon

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,7 @@ module.exports = {
     title: 'Têxtil na Web',
     description: 'Documentação e helpers do portal Têxtil na Web.',
     head: [
-        ['link', { rel: "shortcut icon", href: "https://static.textilnaweb.com/imgs/favicon.png"}],
+        ['link', { rel: "shortcut icon", href: "https://static.textilnaweb.com/imgs/favicon_wire.png"}],
     ],
     locales: {
         '/': {
